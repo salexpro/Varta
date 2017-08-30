c1bc333e4821860c963e46d5790d17a86881681e
salexpro <dev@salex.pro>
2017-08-30 20:39:20 +0300

    desktop header

--- public/js/app.js
+++ public/js/app.js
@@ -6,17 +6,35 @@ var s = skrollr.init({
         box: '150p'
     }
 });
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        nav: false,
-        loop: true,
-        items: 1,
-        autoplay: true,
-        autoplayTimeout: 2500,
-        autoplayHoverPause: false
-    })
+
+// Шапка
+$(window).scroll(function(){
+    if($(window).scrollTop() > 20){
+        $('.header').addClass('minimized');
+    } else {
+        $('.header').removeClass('minimized');
+    }
+})
+$('#navbar a').click(function(e){
+    e.preventDefault();
+    var target = $(this).attr('href').substr(1),
+        offset = $('#' + target).offset().top;
+    $('html, body').animate({ 
+        scrollTop: offset
+    }, 500);
 });
-// Создает обработчик события window.onLoad
+
+// Призы
+$(".owl-carousel").owlCarousel({
+    nav: false,
+    loop: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: false
+})
+
+// Карта
 YMaps.jQuery(function() {
     // Создает экземпляр карты и привязывает его к созданному контейнеру
     var map = new YMaps.Map(YMaps.jQuery(".map_container")[0]);
