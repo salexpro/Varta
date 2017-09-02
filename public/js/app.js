d6e916bde5de452de05d0e5946fdc0f442489250
salexpro <dev@salex.pro>
2017-09-02 05:16:55 +0300

    prfile desktop

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
-var s = skrollr.init({
-    forceHeight: false,
-    constants: {
-        //fill the box for a "duration" of 150% of the viewport (pause for 150%)
-        //adjust for shorter/longer pause
-        box: '150p'
-    }
-});
+if($('#scroll').length){
+    var s = skrollr.init({
+        forceHeight: false,
+        constants: {
+            //fill the box for a "duration" of 150% of the viewport (pause for 150%)
+            //adjust for shorter/longer pause
+            box: '150p'
+        }
+    });
+}
 
 // Шапка
 $(window).scroll(function(){
@@ -26,19 +28,23 @@ $('#navbar a:not([data-toggle]), .modal_form_footer a:not([data-toggle]), .logo'
 });
 
 // Призы
-$(".owl-carousel").owlCarousel({
-    nav: false,
-    loop: true,
-    items: 1,
-    autoplay: true,
-    autoplayTimeout: 2500,
-    autoplayHoverPause: false
-})
+if($('.owl-carousel').length){
+    $('.owl-carousel').owlCarousel({
+        nav: false,
+        loop: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: false
+    })
+}
 
 // Карта
-// YMaps.jQuery(function() {
-//     // Создает экземпляр карты и привязывает его к созданному контейнеру
-//     var map = new YMaps.Map(YMaps.jQuery(".map_container")[0]);
-//     // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
-//     map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
-// })
+if($('.map_container').length){
+    YMaps.jQuery(function() {
+        // Создает экземпляр карты и привязывает его к созданному контейнеру
+        var map = new YMaps.Map(YMaps.jQuery(".map_container")[0]);
+        // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
+        map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
+    })
+}
